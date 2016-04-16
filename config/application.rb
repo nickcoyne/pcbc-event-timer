d97c4f947a1b7aaaebb1f7eab28421d85b104805
Nick <nick@codevader.com>
2016-04-17 10:18:17 +1200

    Hardcode tz to NZ

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,9 @@ module PcbcEventTimer
       #{config.root}/app/models/concerns
       #{config.root}/app/services/concerns
     )
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    config.time_zone = 'Auckland'
   end
 end
